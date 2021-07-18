58a58225885106732062adf81083571c8d736251
Michael-Meade <mameade@utica.edu>
2021-07-18 16:58:00 -0400

    added TwoMiners

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 * minexmr.com
 * hashvault.pro
 * nanopool.org
+
+
+### Purpose
+This tool purpose is to allow the user to search a buncn of different Monero mining pools for a certain address. The script will then add up Monero that was mined and display the results. When I dig into files that were dropped on my honey pot, I often run into malware that tries to mine Monero on my VPS. This script could be used to get a better idea on how much profit the malicious miner has mined using its victims machines.
--- test.rb
+++ test.rb
@@ -17,14 +17,41 @@ module Pools
             main    = JSON.parse(rsp)
             results = {}
             if main["status"] != false || main["error"] != "Account not found"
-                results["balance"]             = main["data"]["balance"],
+                results["balance"]             = main["data"]["balance"]
                 results["paid"]                = main["data"]["paid"]
                 results["hashrate"]            = main["data"]["hashrate"]
+                results["total"]               = main["data"]["balance"].to_f + main["data"]["paid"].to_f
                 results["name"]                = "nanopool"
             end
         return results if !results.empty?
         end
 
+    end
+    class TwoMiners
+        def initialize(address)
+            @address = address
+        end
+        def address
+            @address
+        end
+        def url
+            "https://xmr.2miners.com/api/accounts//#{address}"
+        end
+        def get
+            rsp      = HTTParty.get(url).response.body                
+            main     = JSON.parse(rsp)
+            stats    =  main["stats"]
+            results  = {}
+            paid     = stats["paid"].to_f / 1000000000000
+            bal      = stats["balance"].to_f / 1000000000000
+            results["paid"]     =  paid
+            # un paid
+            results["balance"]  = bal
+            results["total"]    = bal.to_f + paid.to_f
+            results["hashrate"] = main["hashrate"]
+            results["name"]     = "TwoMiners"
+        return results
+        end
     end
     class HashVaultPro
         def initialize(address)
@@ -49,7 +76,7 @@ module Pools
             results["hashrate"] =  hashrate
             results["name"]     = "HashVaultPro"
             # paid + balance 
-            results["total"]    = paid += bal
+            results["total"]    = bal.to_f + paid.to_f
         return results if !results.empty?
         end
     end
@@ -67,10 +94,14 @@ module Pools
             rsp     = HTTParty.get(url).response.body
             main    = JSON.parse(rsp).shift
             results = {}
-            results["balance"]  = main["balance"].to_f / 1000000000
-            results["paid"]     = main["paid"].to_f / 1000000000000
+            balance = main["balance"].to_f / 1000000000000
+            # 1000000000
+            paid    = main["paid"].to_f / 1000000000000
+            results["balance"]  = balance
+            results["paid"]     = 
             results["hashrate"] = main["hashrate"]
             results["name"]     = "xmrmine.com"
+            results["total"]    = balance.to_f + paid.to_f
         return results
         end
     end
@@ -88,24 +119,30 @@ module Pools
             rsp     = HTTParty.get(url).response.body
             main    = JSON.parse(rsp)
             results = {}
-            results["balance"]  = main["amtDue"].to_f / 100000
-            results["paid"]     = main["amtPaid"].to_f / 1000000000000
+            bal     = main["amtDue"].to_f / 1000000000000
+            paid    = main["amtPaid"].to_f / 1000000000000
+            results["balance"]  = bal
+            results["paid"]     = paid
             results["hashrate"] = main["hash"]
+            results["total"]    = bal + paid
             results["name"]     = "supportxmr.com"
         return results
         end
     end
 end
-def get_all
+def get_all(addr)
     t = 0
     Pools.constants.select do |c|
-        k = Pools.const_get(c).new("49ubSTdDp9hPmYE7paRM6PZFLmqvsedZ56MXLUT8mvYnTzjVCKGDbpuW4RVdvZon228uWnkjoJN8S6w5S4LdgeK8UBMMEhJ").get
+        k = Pools.const_get(c).new(addr).get
+        p k
         if !k.nil?
-           bal = k["balance"].to_f
-           t += bal
+            puts k["total"]
+           t += k["total"]
         end
     end
-puts t
+puts "Address: #{addr}"
+
+puts "Balance: #{t}"
 end
 
-get_all
+get_all("49ubSTdDp9hPmYE7paRM6PZFLmqvsedZ56MXLUT8mvYnTzjVCKGDbpuW4RVdvZon228uWnkjoJN8S6w5S4LdgeK8UBMMEhJ")
